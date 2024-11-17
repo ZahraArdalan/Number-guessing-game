506d2ca41a141c6b47d1e645d3dc1a3adfb6d975
zahra Ardalan <z.ardalan78@gmail.com>
2024-11-17 16:59:42 +0330

    finaly

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const SecretNumber = Math.trunc(Math.random() * 20) + 1;
+let score=20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -10,5 +11,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === SecretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
-  }else if(guess > SecretNumber)
+  }else if(guess > SecretNumber){
+    document.querySelector('.message').textContent='📈Too high!';
+    score--;
+    document.querySelector('.score').textContent=score;
+  }else if(guess < SecretNumber){
+    document.querySelector('.message').textContent='📉Too low!';
+    score--
+    document.querySelector('.score').textContent=score;}
 });
